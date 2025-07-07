9d5f605f4e883ef86787baf7fa1ff3f5e0ceb7de
ash sinha <anshusinha036@gmail.com>
2025-07-07 18:42:11 +0530

    main

---
+++ script.js
@@ -0,0 +1,20 @@
+const toggleBtn = document.getElementById("toggleBtn");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // Optional: Save preference to localStorage
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+// Load saved theme on page load
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+  }
+})
